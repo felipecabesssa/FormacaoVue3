13e1391aea24319f964ac5c190ebfc42cd8372d3
felipecabesssa <felipesoares100@hotmail.com>
2022-03-30 00:21:40 -0300

    V-if - Mensagem se lista estiver vazia

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,12 @@
     <div class="column is-three-quarter">
       <Formulario @aoSalvarTarefa="salvarTarefa" />
       <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        <Tarefa
+          v-for="(tarefa, index) in tarefas"
+          :key="index"
+          :tarefa="tarefa"
+        />
+        <Box v-if="listaEstaVazia"> Você não está muito produtivo hoje :( </Box>
       </div>
     </div>
   </main>
@@ -19,6 +24,7 @@ import BarraLateral from "./components/BarraLateral.vue";
 import Formulario from "./components/Formulario.vue";
 import Tarefa from "./components/Tarefa.vue";
 import ITarefa from "./interfaces/ITarefa";
+import Box from "./components/Box.vue";
 
 export default defineComponent({
   name: "App",
@@ -26,17 +32,23 @@ export default defineComponent({
     BarraLateral,
     Formulario,
     Tarefa,
+    Box,
   },
   data() {
     return {
       tarefas: [] as ITarefa[],
     };
   },
-  methods: {
-    salvarTarefa (tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
+  computed: {
+    listaEstaVazia() : boolean {
+      return this.tarefas.length === 0
     }
-  }
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa);
+    },
+  },
 });
 </script>
 
