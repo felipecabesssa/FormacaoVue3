eb339f6ab833261e510d0a471c5614f4adff19dd
felipecabesssa <felipesoares100@hotmail.com>
2022-03-29 22:06:36 -0300

    Refatorando o codigo, isolando o cronometro e passando o tempoEmSegundos via Props

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -17,11 +17,9 @@
         <div
           class="is-flex is-align-items-center is-justify-content-space-between"
         >
-          <section>
-            <strong>
-              {{ tempoDecorrido }}
-            </strong>
-          </section>
+
+          <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+
           <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
@@ -42,20 +40,22 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Cronometro from './Cronometro.vue'
 
 export default defineComponent({
   name: "Formulario",
+
+  components: {
+    Cronometro
+  },
+
   data(){
     return{
       tempoEmSegundos: 0,
       cronometro: 0
     }
   },
-  computed: {
-    tempoDecorrido() : string {
-      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
-    }
-  },
+
   methods: {
     iniciar(){
       this.cronometro = setInterval(() => {
@@ -66,5 +66,6 @@ export default defineComponent({
       clearInterval(this.cronometro)
     }
   }
+
 });
 </script>
