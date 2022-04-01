e6174bc6357d5ff941d72594c1dc976381cbd35b
felipecabesssa <felipesoares100@hotmail.com>
2022-03-31 23:27:55 -0300

    Adicionando Notificações - Classes de alert do Bulma

---
+++ src/components/Notificacoes.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="notificacoes">
+        <article class="message is-success">
+            <div class="message-header">Atenção</div>
+            <div class="message-body">Aqui vai a notificação</div>
+        </article>
+
+        <article class="message is-warning">
+            <div class="message-header">Atenção</div>
+            <div class="message-body">Aqui vai a notificação</div>
+        </article>
+
+        <article class="message is-danger">
+            <div class="message-header">Atenção</div>
+            <div class="message-body">Aqui vai a notificação</div>
+        </article>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "Notificacoes",
+});
+</script>
+
+<style scoped>
+.notificacoes {
+    position: absolute;
+    right: 0;
+    width: 250px;
+    padding: 8px;
+    z-index: 1;
+}
+</style>
