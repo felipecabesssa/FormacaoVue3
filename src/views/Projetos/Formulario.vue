a833d8ed2d7577b965ab627e10e923b4c5a35399
felipecabesssa <felipesoares100@hotmail.com>
2022-03-31 23:52:10 -0300

    Removendo a notificacao automaticamente após 3 segundos

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -25,7 +25,8 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useStore } from "@/store";
-import { ALTERA_PROJETO, ADICIONA_PROJETO } from "@/store/tipo-mutacoes";
+import { ALTERA_PROJETO, ADICIONA_PROJETO, NOTIFICAR } from "@/store/tipo-mutacoes";
+import { TipoNotificacao } from "@/interfaces/INotificacao";
 
 export default defineComponent({
     name: "Formulario",
@@ -56,6 +57,11 @@ export default defineComponent({
                 this.store.commit(ADICIONA_PROJETO, this.nomeDoProjeto)
             }
             this.nomeDoProjeto = '';
+            this.store.commit(NOTIFICAR, {
+                titulo: 'Projeto salvo',
+                texto: 'Foi =)',
+                tipo: TipoNotificacao.SUCESSO
+            })
             this.$router.push('/projetos')
         }
     },
