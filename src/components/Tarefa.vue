32c1724617a1816ef7d1186d9d35b00427e776f5
felipecabesssa <felipesoares100@hotmail.com>
2022-04-01 00:29:29 -0300

    Vinculando Tarefas ao Projeto e exibindo na lista

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,9 +1,12 @@
 <template>
   <Box>
     <div class="columns">
-      <div class="column is-7 corTexto">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
+      <div class="column is-4 corTexto">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
       <div class="column">
         <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
+      </div>
+      <div class="column is-3">
+          {{ tarefa.projeto?.nome || 'N/D' }}
       </div>
     </div>
   </Box>
