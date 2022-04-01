abb8a48cc06faeff3184e82cf718b6ed3fc7e5f4
felipecabesssa <felipesoares100@hotmail.com>
2022-03-31 22:20:52 -0300

    Editando projetos - Botões/Router-links para add projeto e editar

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,35 +1,34 @@
 <template>
     <section class="projetos">
         <h1 class="title">Projetos</h1>
-        <form @submit.prevent="salvar">
-            <div class="field">
-                <label for="nomeDoProjeto" class="label">
-                    Nome do projeto
-                </label>
-                <input
-                    type="text"
-                    class="input"
-                    v-model="nomeDoProjeto"
-                    id="nomeDoProjeto"
-                />
-            </div>
-            <div class="field">
-                <button class="button" type="submit">
-                    Salvar
-                </button>
-            </div>
-        </form>
+        <router-link to="/projetos/novo" class="button">
+            <span class="icon is-small">
+                <i class="fas fa-plus"></i>
+            </span>
+            <span>Novo projeto</span>
+        </router-link>
         <table class="table is-fullwidth mt-4">
             <thead>
                 <tr>
                     <th class="tituloTabela">ID</th>
                     <th class="tituloTabela">Nome</th>
+                    <th class="tituloTabela">Ações</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="projeto in projetos" :key="projeto.id">
                     <td>{{ projeto.id }}</td>
                     <td>{{ projeto.nome }}</td>
+                    <td>
+                        <router-link
+                            :to="`/projetos/${projeto.id}`"
+                            class="button"
+                        >
+                            <span class="icon is-small">
+                                <i class="fas fa-pencil-alt"></i>
+                            </span>
+                        </router-link>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -42,32 +41,21 @@ import { useStore } from "@/store";
 
 export default defineComponent({
     name: "Projetos",
-    data() {
-        return {
-            nomeDoProjeto: '',
-        }
-    },
-    methods: {
-        salvar() {
-            this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
-            this.nomeDoProjeto = ''
-        }
-    },
     setup() {
-        const store = useStore()
+        const store = useStore();
         return {
-            store,
-            projetos: computed(() => store.state.projetos)
-        }
-    }
+            projetos: computed(() => store.state.projetos),
+        };
+    },
 });
 </script>
 
 <style scoped>
-.projetos{
+.projetos {
     padding: 1.25rem;
 }
-.title, .label{
+.title,
+.label {
     color: var(--texto-primario);
 }
 .table {
