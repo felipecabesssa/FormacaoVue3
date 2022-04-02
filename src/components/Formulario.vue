61dcc9e8d0f4ab7ce9cf55eb3884d73ce41f052c
felipecabesssa <felipesoares100@hotmail.com>
2022-04-01 23:47:33 -0300

    Refatorando o Formulario do component

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -38,7 +38,7 @@
 
 <script lang="ts">
 import { key } from "@/store";
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import { useStore } from "vuex";
 import Temporizador from "./Temporizador.vue";
 
@@ -48,26 +48,28 @@ export default defineComponent({
     components: {
         Temporizador,
     },
-    data() {
-        return {
-            descricao: '',
-            idProjeto: ''
-        };
-    },
-    methods: {
-        finalizarTarefa(tempoDecorrido: number): void {
-            this.$emit("aoSalvarTarefa", {
-                duracaoEmSegundos: tempoDecorrido,
-                descricao: this.descricao,
-                projeto: this.projetos.find(proj => proj.id == this.idProjeto)
-            });
-            this.descricao = "";
-        },
-    },
-    setup() {
+    setup(props, { emit }) {
         const store = useStore(key);
+
+        const descricao = ref("")
+        const idProjeto = ref("")
+
+        const projetos = computed(() => store.state.projeto.projetos)
+
+        const finalizarTarefa = (tempoEmSegundos: number): void => {
+            emit("aoSalvarTarefa", {
+                duracaoEmSegundos: tempoEmSegundos,
+                descricao: descricao.value,
+                projeto: projetos.value.find(proj => proj.id == idProjeto.value)
+            })
+            descricao.value = ''
+        }
+
         return {
-            projetos: computed(() => store.state.projeto.projetos),
+            descricao,
+            idProjeto,
+            projetos,
+            finalizarTarefa
         };
     },
 });
