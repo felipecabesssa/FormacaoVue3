f4adb30ba92e3688c11da8048d1d534b5a6d01d1
felipecabesssa <felipesoares100@hotmail.com>
2022-04-01 21:29:21 -0300

    Fechando o modal apos editar e clicar em salvar

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,6 +6,7 @@ import {
     ADICIONA_PROJETO,
     ADICIONA_TAREFA,
     ALTERA_PROJETO,
+    ALTERA_TAREFA,
     DEFINIR_PROJETOS,
     DEFINIR_TAREFAS,
     EXCLUIR_PROJETO,
@@ -18,6 +19,7 @@ import {
     REMOVER_PROJETO,
     OBTER_TAREFAS,
     CADASTRAR_TAREFA,
+    ALTERAR_TAREFA,
 } from "./tipos-acoes";
 import http from "@/http";
 import ITarefa from "@/interfaces/ITarefa";
@@ -62,6 +64,10 @@ export const store = createStore<Estado>({
         [ADICIONA_TAREFA](state, tarefa: ITarefa) {
             state.tarefas.push(tarefa);
         },
+        [ALTERA_TAREFA](state, tarefa: ITarefa) {
+            const index = state.tarefas.findIndex(t => t.id == tarefa.id);
+            state.tarefas[index] = tarefa;
+        },
         [NOTIFICAR](state, novaNotificacao: INotificacao) {
             novaNotificacao.id = new Date().getTime();
             state.notificacoes.push(novaNotificacao);
@@ -99,6 +105,11 @@ export const store = createStore<Estado>({
         [CADASTRAR_TAREFA]({ commit }, tarefa: ITarefa) {
             return http.post("/tarefas", tarefa)
                 .then(resposta => commit(ADICIONA_TAREFA, resposta.data))
+        },
+        [ALTERAR_TAREFA]({ commit }, tarefa: ITarefa) {
+            return http.put(`/tarefas/${tarefa.id}`, tarefa)
+                .then(() => commit(ALTERA_TAREFA, tarefa))
+
         },
     },
 });
--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -37,7 +37,7 @@
                     </div>
                 </section>
                 <footer class="modal-card-foot">
-                    <button class="button is-success">Salvar alterações</button>
+                    <button @click="alterarTarefa" class="button is-success">Salvar alterações</button>
                     <button @click="fecharModal" class="button">
                         Cancelar
                     </button>
@@ -53,7 +53,7 @@ import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
 import { useStore } from "@/store";
-import { OBTER_PROJETOS, OBTER_TAREFAS } from "@/store/tipos-acoes";
+import { ALTERAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS } from "@/store/tipos-acoes";
 import { CADASTRAR_TAREFA } from "@/store/tipos-acoes";
 import ITarefa from "@/interfaces/ITarefa";
 
@@ -94,6 +94,10 @@ export default defineComponent({
         fecharModal() {
             this.tarefaSelecionada = null;
         },
+        alterarTarefa() {
+            this.store.dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
+                .then(() => this.fecharModal())
+        }
     },
 });
 </script>
